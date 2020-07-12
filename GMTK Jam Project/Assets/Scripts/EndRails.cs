535c67fc24aaa61b16d355ec6060d7e82df01fca
JatinsticGaming <jatinim.chess@gmail.com>
2020-07-12 09:37:53 +0530

    Changes

---
+++ GMTK Jam Project/Assets/Scripts/EndRails.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndRails : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Train"))
+        {
+            // End the Level...
+            SceneManager.LoadScene("Win Screen");
+        }
+    }
+}
