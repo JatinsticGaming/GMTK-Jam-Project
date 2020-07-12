4539e39f8fa445caaf97af3d5338ccf545ba458a
JatinsticGaming <jatinim.chess@gmail.com>
2020-07-12 22:27:22 +0530

    Some Changes

--- GMTK Jam Project/Assets/Scripts/EndRails.cs
+++ GMTK Jam Project/Assets/Scripts/EndRails.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class EndRails : MonoBehaviour
 {
+    public Canvas WinScreen;
+    private Animator[] AnimatorsFound;
+    public Animator WinScreenBackgroundAnim;
+    public Animator UIAnim;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        AnimatorsFound = FindObjectsOfType<Animator>();
+        foreach(Animator other in AnimatorsFound)
+        {
+            if(other.name == "Background")
+            {
+                WinScreenBackgroundAnim = other;
+            }
+            else if(other.name == "UI")
+            {
+                UIAnim = other;
+            }
+        }
     }
     
     // Update is called once per frame
@@ -22,7 +39,8 @@ public class EndRails : MonoBehaviour
         if (other.CompareTag("Train"))
         {
             // End the Level...
-            SceneManager.LoadScene("Win Screen");
+            WinScreenBackgroundAnim.SetTrigger("FadeIn");
+            UIAnim.SetTrigger("ComeDown");
         }
     }
 }
